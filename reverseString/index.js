702fa9f95abadd44d2963f67be7491274fa4c8c9
syukronarie <syukronarie@gmail.com>
2021-02-18 20:38:29 +0700

    Finish implementing Reverse String

--- reverseString/index.js
+++ reverseString/index.js
@@ -4,7 +4,25 @@
 //   reverse('abcd') === 'dcba'
 //   reverse('Hello!') === '!olleH'
 
-function reverse(str) {}
+function reverse(str) {
+	// solution #1 using readable func
+	// return str.split("").reverse().join("");
+
+	// solution #2 using for loop
+	// let res = "";
+	// for (let i = 0; i < str.length; i++) {
+	// let char = str[i]
+	// 	res = char + res;
+	// 	console.log(res);
+	// }
+	// return res;
+
+	// solution #3 using ES6
+	return str.split("").reduce((output, char) => {
+		output = char + output;
+		return output;
+	}, "");
+}
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
 // \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
@@ -29,13 +47,13 @@ function reverse(str) {}
 //                          ______ ______ ______ ______ ______
 //                         |______|______|______|______|______|
 
-mocha.setup('bdd');
+mocha.setup("bdd");
 const { assert } = chai;
 
-describe('String Reversal', () => {
-	it('reverse() correctly reverses string', () => {
-		assert.equal(reverse('ffaa'), 'aaff');
-		assert.equal(reverse('  aaff'), 'ffaa  ');
+describe("String Reversal", () => {
+	it("reverse() correctly reverses string", () => {
+		assert.equal(reverse("ffaa"), "aaff");
+		assert.equal(reverse("  aaff"), "ffaa  ");
 	});
 });
 
