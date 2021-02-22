6d16e75649093f721ddd4ecc0949e4ff30f5737d
syukronarie <syukronarie@gmail.com>
2021-02-22 20:27:18 +0700

    finish implementing anagrams solution two

--- anagrams/index.js
+++ anagrams/index.js
@@ -21,31 +21,40 @@ function anagrams(stringA, stringB) {
    *  */
 
 	// Solution #1
+	// stringA = stringA.toLowerCase().replace(/[\W_]+/g, "");
+	// stringB = stringB.toLowerCase().replace(/[\W_]+/g, "");
+
+	// if (stringA.length !== stringB.length) {
+	// 	return false;
+	// }
+
+	// const stringACharCount = {};
+
+	// for (let i = 0; i < stringA.length; i++) {
+	// 	const aChar = stringA[i];
+	// 	stringACharCount[aChar] = stringACharCount[aChar] + 1 || 1;
+	// }
+
+	// for (let i = 0; i < stringB.length; i++) {
+	// 	const bChar = stringB[i];
+
+	// 	if (!stringACharCount[bChar]) {
+	// 		return false;
+	// 	} else {
+	// 		stringACharCount[bChar]--;
+	// 	}
+	// }
+
+	// return true;
+
+	// Solution #2
 	stringA = stringA.toLowerCase().replace(/[\W_]+/g, "");
 	stringB = stringB.toLowerCase().replace(/[\W_]+/g, "");
 
-	if (stringA.length !== stringB.length) {
-		return false;
-	}
+	const stringASorted = stringA.split("").sort().join("");
+	const stringBSorted = stringB.split("").sort().join("");
 
-	const stringACharCount = {};
-
-	for (let i = 0; i < stringA.length; i++) {
-		const aChar = stringA[i];
-		stringACharCount[aChar] = stringACharCount[aChar] + 1 || 1;
-	}
-
-	for (let i = 0; i < stringB.length; i++) {
-		const bChar = stringB[i];
-
-		if (!stringACharCount[bChar]) {
-			return false;
-		} else {
-			stringACharCount[bChar]--;
-		}
-	}
-
-	return true;
+	return stringASorted === stringBSorted;
 }
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
