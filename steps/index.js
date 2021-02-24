b48071fd075570794f0ff9e19321154db6bd6e58
syukronarie <syukronarie@gmail.com>
2021-02-24 16:40:42 +0700

    finish implementation steps solution one

--- steps/index.js
+++ steps/index.js
@@ -16,7 +16,22 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+	// solution #1
+	for (let i = 1; i <= n; i++) {
+		let step = "";
+
+		for (let hash = i; hash > 0; hash--) {
+			step += "#";
+		}
+
+		for (let space = n - i; space > 0; space--) {
+			step += " ";
+		}
+
+		console.log(step);
+	}
+}
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
 // \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
@@ -46,13 +61,13 @@ const { assert } = chai;
 console.log = sinon.spy();
 
 describe("Steps", () => {
-  it("steps() works", () => {
-    steps(3);
-    assert.equal(console.log.callCount, 3);
-    assert.equal(console.log.getCall(0).args[0], "#  ");
-    assert.equal(console.log.getCall(1).args[0], "## ");
-    assert.equal(console.log.getCall(2).args[0], "###");
-  });
+	it("steps() works", () => {
+		steps(3);
+		assert.equal(console.log.callCount, 3);
+		assert.equal(console.log.getCall(0).args[0], "#  ");
+		assert.equal(console.log.getCall(1).args[0], "## ");
+		assert.equal(console.log.getCall(2).args[0], "###");
+	});
 });
 
 mocha.run();
