769081a32d50fa47fccb03150edd5a3397178252
syukronarie <syukronarie@gmail.com>
2021-02-24 22:01:19 +0700

    finish add solution two on steps

--- steps/index.js
+++ steps/index.js
@@ -18,16 +18,26 @@
 
 function steps(n) {
 	// solution #1
+	// for (let i = 1; i <= n; i++) {
+	// 	let step = "";
+
+	// 	for (let hash = i; hash > 0; hash--) {
+	// 		step += "#";
+	// 	}
+
+	// 	for (let space = n - i; space > 0; space--) {
+	// 		step += " ";
+	// 	}
+
+	// 	console.log(step);
+	// }
+
+	// solution #2
 	for (let i = 1; i <= n; i++) {
 		let step = "";
 
-		for (let hash = i; hash > 0; hash--) {
-			step += "#";
-		}
-
-		for (let space = n - i; space > 0; space--) {
-			step += " ";
-		}
+		step += "#".repeat(i);
+		step += " ".repeat(n - i);
 
 		console.log(step);
 	}
