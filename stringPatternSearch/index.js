723f48df3636c9dc5242aaf3e1e268e8ee965ef2
syukronarie <syukronarie@gmail.com>
2021-03-01 06:27:12 +0700

    Finish Implementing String Pattern Search

--- stringPatternSearch/index.js
+++ stringPatternSearch/index.js
@@ -3,7 +3,25 @@
 // stringPatternSearch("lollipop", "lol") --> 1
 // stringPatternSearch("lolol", "lol") --> 2
 
-function stringPatternSearch(text, pattern) {}
+function stringPatternSearch(text, pattern) {
+	let count = 0;
+
+	for (let i = 0; i < text.length; i++) {
+		for (let j = 0; j < pattern.length; j++) {
+			const patternChar = pattern[j];
+
+			if (patternChar !== text[i + j]) {
+				break;
+			}
+
+			if (j === pattern.length - 1) {
+				count++;
+			}
+		}
+	}
+
+	return count;
+}
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
 // \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
@@ -32,10 +50,10 @@ mocha.setup("bdd");
 const { assert } = chai;
 
 describe("stringPatternSearch()", () => {
-  it("works.", () => {
-    assert.equal(stringPatternSearch("lollipop", "lol"), 1);
-    assert.equal(stringPatternSearch("lolol", "lol"), 2);
-  });
+	it("works.", () => {
+		assert.equal(stringPatternSearch("lollipop", "lol"), 1);
+		assert.equal(stringPatternSearch("lolol", "lol"), 2);
+	});
 });
 
 mocha.run();
